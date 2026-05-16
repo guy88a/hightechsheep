8068f56ea9bd96781298cdee41f6f6b71f4e2603
guy88a <guy88a@gmail.com>
2026-05-17 00:29:19 +0300

    cc: remove unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Manrope, Mulish, Bebas_Neue } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import MainLayout from "./_components/layout/MainLayout";
 import "./globals.css";
 import "./_styles/layout/layout.scss";
@@ -15,12 +15,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const firaSans = Manrope({
-  variable: "--font-fira-sans",
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-});
-
 export const metadata: Metadata = {
   title: "Hightech Sheep",
   description: "Previous Generation Gaming",
@@ -34,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${firaSans.variable}`}
+        className={`${geistSans.variable} ${geistMono.variable}`}
       >
         <MainLayout>{children}</MainLayout>
       </body>
